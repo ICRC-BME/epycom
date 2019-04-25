56d516353e4bd0abd13c87aa27b95d8f3863e6b9
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-04-25 11:57:42 +0200

    Univariate tests now binary independent.

--- epycom/univariate/tests/test_univariate.py
+++ epycom/univariate/tests/test_univariate.py
@@ -3,11 +3,8 @@
 # Research Center, Biomedical Engineering. All Rights Reserved.
 # Distributed under the (new) BSD License. See LICENSE.txt for more info.
 
-import pytest
-
 
 # Std imports
-import pickle
 
 # Third pary imports
 import pytest
@@ -19,32 +16,32 @@ from epycom.univariate.univariate_methods import (compute_signal_stats,
                                                   comute_pse)
 
 
-def test_signal_stats(get_ieeg_data):
-    ch = get_ieeg_data['data'][-1]
+def test_signal_stats(create_testing_data):
+    ch = create_testing_data
     stats = compute_signal_stats(ch)
 
-    expected_vals = [6863666.519704277,
-                     4022165.30204,
-                     2119936.0,
-                     187717401.0,
+    expected_vals = [6.68954,
+                     5.0,
+                     2.32213,
+                     67.65263,
                      0.0,
-                     625681.0,
-                     5475600.0]
+                     0.49719,
+                     7.05092]
 
     for exp_stat, stat in zip(expected_vals, stats):
-        assert round(exp_stat, 5) == round(stat, 5)
+        assert round(stat, 5) == exp_stat
 
 
-def test_fac(get_ieeg_data):
-    ch = get_ieeg_data['data'][-1]
-    assert round(compute_fac(ch, 5000), 5) == round(-0.24878536953248276, 5)
+def test_fac(create_testing_data):
+    ch = create_testing_data
+    assert round(compute_fac(ch, 5000), 5) == -0.00019
 
 
-def test_pac(get_ieeg_data):
-    ch = get_ieeg_data['data'][-1]
-    assert round(compute_pac(ch, 5000), 5) == round(0.30791423722563455, 5)
+def test_pac(create_testing_data):
+    ch = create_testing_data
+    assert round(compute_pac(ch, 5000), 5) == 0.01189
 
 
-def test_pse(get_ieeg_data):
-    ch = get_ieeg_data['data'][-1]
-    assert round(comute_pse(ch), 5) == round(7.639563163873471, 5)
+def test_pse(create_testing_data):
+    ch = create_testing_data
+    assert round(comute_pse(ch), 5) == 4.32193
