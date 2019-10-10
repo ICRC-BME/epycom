00dda15e38c2825c81e19de5ce84caedd6ddc776
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-10-10 12:15:12 +0200

    Resolve overlooked conflicts

--- epycom/bivariate/tests/test_bivariate.py
+++ epycom/bivariate/tests/test_bivariate.py
@@ -62,23 +62,7 @@ def test_phase_const(create_testing_data, benchmark):
 def test_pli(create_testing_data, benchmark):
     lag = int((5000 / 100) / 2)
     lag_step = int(lag / 10)
-<<<<<<< HEAD
     compute_instance = PhaseLagIndex(lag=lag, lag_step=lag_step)
     res = benchmark(compute_instance.run_windowed,
                     create_testing_data, 50000)
     assert isclose(res[0][2], 1.0, abs_tol=10-6)
-=======
-    assert (round(compute_pli(ch_1, ch_2, lag, lag_step)[0][0], 5)
-            == 1.0)
-
-
-def test_nonlinear_correlation(create_testing_data):
-    noncorr, tau2 = compute_nonlinear_corr(create_testing_data[0], create_testing_data[1], 200, 20, 2500, 250,  100)
-    assert (round(noncorr[0], 5) == 0.23222)
-    noncorr, tau2 = compute_nonlinear_corr(create_testing_data[0], create_testing_data[1], )
-    assert (round(noncorr[0], 5) == 0.00242)
-    noncorr, tau2 = compute_nonlinear_corr(create_testing_data[0], abs(create_testing_data[0]), )
-    assert (round(noncorr[0], 5) == 0.9952)
-    noncorr, tau2 = compute_nonlinear_corr(abs(create_testing_data[0]), create_testing_data[0])
-    assert (round(noncorr[0], 5) == 0.0)
->>>>>>> origin/master
