8580c8d2fc9e576e5f250e11c8c7e1c5b80edd09
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-10-10 11:45:34 +0200

    Remove fce and pce from __init__.py

--- epycom/univariate/__init__.py
+++ epycom/univariate/__init__.py
@@ -3,5 +3,3 @@ from .hjorth_mobility import compute_hjorth_mobility, HjorthMobility
 from .hjorth_complexity import compute_hjorth_complexity, HjorthComplexity
 from .lyapunov_exponent import compute_lyapunov_exponent, LyapunovExponent
 from .power_spectral_entropy import compute_pse, PowerSpectralEntropy
-from .univariate_methods import compute_fac
-from .univariate_methods import compute_pac
