7bd58cf6479e8901218a77a34f1603f47a7c3f5b
Jan Cimbalnik <jan.cimbalnik@fnusa.cz>
2019-11-10 11:05:12 +0100

    Adjust tests to reflect previous commit

--- epycom/univariate/tests/test_univariate.py
+++ epycom/univariate/tests/test_univariate.py
@@ -30,7 +30,7 @@ def test_signal_stats(create_testing_data, benchmark):
                      0.49719,
                      7.05092)
 
-    for exp_stat, stat in zip(expected_vals, list(stats[0])[2:]):
+    for exp_stat, stat in zip(expected_vals, list(stats[0])[:-1]):
         assert isclose(stat, exp_stat, abs_tol=10e-6)
 
 
@@ -38,25 +38,25 @@ def test_pse(create_testing_data, benchmark):
     compute_instance = PowerSpectralEntropy()
     res = benchmark(compute_instance.run_windowed,
                     create_testing_data, 50000)
-    assert isclose(res[0][2], 4.32193, abs_tol=10e-6)
+    assert isclose(res[0][0], 4.32193, abs_tol=10e-6)
 
 
 def test_lyapunov_exponent(create_testing_data, benchmark):
     compute_instance = LyapunovExponent(sample_lag=25)
     res = benchmark(compute_instance.run_windowed,
                     create_testing_data[:5000], 5000)
-    assert isclose(res[0][2], 5.794813, abs_tol=10e-6)
+    assert isclose(res[0][0], 5.794813, abs_tol=10e-6)
 
 
 def test_hjorth_mobility(create_testing_data, benchmark):
     compute_instance = HjorthMobility(fs=5000)
     res = benchmark(compute_instance.run_windowed,
                     create_testing_data, 50000)
-    assert isclose(res[0][2], 3113.283, abs_tol=10e-3)
+    assert isclose(res[0][0], 3113.283, abs_tol=10e-3)
 
 
 def test_hjorth_complexity(create_testing_data, benchmark):
     compute_instance = HjorthComplexity(fs=5000)
     res = benchmark(compute_instance.run_windowed,
                     create_testing_data, 50000)
-    assert isclose(res[0][2], 2.27728, abs_tol=10e-6)
+    assert isclose(res[0][0], 2.27728, abs_tol=10e-6)
